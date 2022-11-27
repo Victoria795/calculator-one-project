87cdcde7081b42b891e0cf2b815223210b88d571
Виктория Шинтябина <vika.shintyabina@mail.ru>
2022-11-28 00:26:42 +0300

    добавлен компонент и логика

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,76 @@
 <template>
 <div class="wrapper">
   <div class="container">
-  <div class="calc-description">120-50+100</div>
-  <div class="calc-result"><hr>170</div>
-  <button class="calc__btn calc__btn_delete" type="button">C</button>
-  <button class="calc__btn calc__btn_sign" type="button">%</button>
-  <button class="calc__btn calc__btn_sign" type="button">&#8656;</button>
-  <button class="calc__btn calc__btn_sign" type="button">&#247;</button>
+  <div class="calc-description">{{currentResult || 0}}</div>
+  <div class="calc-result"><hr>{{finalResult || 0}}</div>
+  <calc-btn :delete="true" symbol="C" @:click = "clean()"/>
+  <calc-btn :sign="true" symbol="%" @:click = "percentage()"/>
+  <calc-btn :sign="true" symbol="&#8656;" @:click = "erase()"/>
+  <calc-btn :sign="true" symbol="&#247;" @:click = "input('/')"/>
 
-  <button class="calc__btn" type="button">7</button>
-  <button class="calc__btn" type="button">8</button>
-  <button class="calc__btn" type="button">9</button>
-  <button class="calc__btn calc__btn_sign" type="button">&#215;</button>
+  <calc-btn symbol="7" @:click = "input(7)"/>
+  <calc-btn symbol="8" @:click = "input(8)"/>
+  <calc-btn symbol="9" @:click = "input(9)"/>
+  <calc-btn :sign="true" symbol="*" @:click = "input('*')"/>
 
-  <button class="calc__btn" type="button">4</button>
-  <button class="calc__btn" type="button">5</button>
-  <button class="calc__btn" type="button">6</button>
-  <button class="calc__btn calc__btn_sign" type="button">-</button>  
+  <calc-btn symbol="4"  @:click = "input(4)"/>
+  <calc-btn symbol="5" @:click = "input(5)"/>
+  <calc-btn symbol="6" @:click = "input(6)"/>
+  <calc-btn :sign="true" symbol="-" @:click = "input('-')"/>
 
-  <button class="calc__btn" type="button">1</button>
-  <button class="calc__btn" type="button">2</button>
-  <button class="calc__btn" type="button">3</button>    
-  <button class="calc__btn calc__btn_sign" type="button">+</button>
+  <calc-btn symbol="1" @:click = "input(1)"/>
+  <calc-btn symbol="2" @:click = "input(2)"/>
+  <calc-btn symbol="3" @:click = "input(3)"/>
+  <calc-btn :sign="true" symbol="+" @:click = "input('+')"/>
 
-  <button class="calc__btn calc__btn_sign" type="button">.</button>
-  <button class="calc__btn" type="button">0</button>
-  <button class="calc__btn calc__btn_equal" type="button">=</button>
+  <calc-btn :sign="true" symbol="." @:click = "dot()"/>
+  <calc-btn symbol="0" @:click = "input(0)"/>
+  <calc-btn :equal="true" symbol="=" @:click = "count()"/>
   </div>
 </div>
 </template> 
 <script>
-/*import HelloWorld from './components/HelloWorld.vue'
+
+import calcBtn from './components/calc-btn.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}*/
+    calcBtn
+  },
+  data() {
+    return{
+    currentResult : '',
+    finalResult: '',
+    }
+  },
+  methods: {
+    input(symbol) {
+      this.currentResult += symbol;
+    },
+    clean() {
+      this.currentResult = '';
+      this.finalResult = '';
+    },
+    erase(){
+      this.currentResult = this.currentResult.slice(0, -1);
+    },
+    dot(){ if(this.currentResult.length === 0 || this.currentResult.endsWith('+') 
+    || this.currentResult.endsWith('-') || this.currentResult.endsWith('*') || this.currentResult.endsWith('/')){
+      this.input('0.');
+    }
+    else{
+      this.input('.');
+    }
+    },
+    percentage(){
+      this.currentResult = this.currentResult/100;
+    },
+    count(){
+      this.finalResult = eval(this.currentResult);
+    },
+}
+}
 </script>
 
 <style>
@@ -87,24 +120,5 @@ hr {
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-}
-.calc__btn{
-  font: normal 700 26px/16px 'Avenir', sans-serif;
-  background: #030d11;
-  border: none;
-  box-shadow: 0px 3px 3px rgb(196, 193, 193);
-  color: white;
-}
-.calc__btn_equal{
-  grid-column: 3 / -1;
-  background: rgb(191, 248, 250);
-  color: #030d11;
-}
-.calc__btn_sign{
-  background: #134c54;
-}
-.calc__btn_delete{
-  background:rgb(191, 248, 250);
-  color: #030d11;
 }
 </style>
---
+++ src/components/calc-btn.vue
@@ -0,0 +1,66 @@
+<template>
+  <button 
+  class="calc__btn"
+  :class="className"
+  type="button"
+  >{{symbol}}</button>
+</template>
+<script>
+//import { computed } from '@vue/reactivity';
+export default {
+  name: 'calc-btn',
+  props: {
+    symbol: String,
+    equal: {
+      type: Boolean,
+      default: false
+    },
+    sign: {
+      type: Boolean,
+      default: false
+    },
+    delete: {
+      type: Boolean,
+      default: false
+    },
+  },
+  data() {
+      
+  },
+  computed: {
+     className() {
+      return {
+        "calc__btn_equal": this.equal,
+        "calc__btn_sign": this.sign,
+        "calc__btn_delete": this.delete
+      }
+     }
+  },
+  methods: {
+      
+  }
+}
+</script>
+
+<style>
+.calc__btn{
+  font: normal 700 26px/16px 'Avenir', sans-serif;
+  background: #030d11;
+  border: none;
+  box-shadow: 0px 3px 3px rgb(196, 193, 193);
+  color: white;
+}
+.calc__btn_equal{
+  grid-column: 3 / -1;
+  background: rgb(191, 248, 250);
+  color: #030d11;
+}
+.calc__btn_sign{
+  background: #134c54;
+}
+.calc__btn_delete{
+  background:rgb(191, 248, 250);
+  color: #030d11;
+}
+
+</style>
